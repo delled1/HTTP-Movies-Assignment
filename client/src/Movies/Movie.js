60a724c2d23335366fb8359974f29dd9888f162b
Eric Della <ericdella13@gmail.com>
2021-01-19 19:42:06 -0800

    delete movie works

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -3,10 +3,11 @@ import axios from "axios";
 import { useHistory, useParams } from "react-router-dom";
 import MovieCard from "./MovieCard";
 
-function Movie({ addToSavedList }) {
+function Movie({ addToSavedList, setMovieList }) {
   const [movie, setMovie] = useState(null);
   const params = useParams();
-  const { push } = useHistory();
+  const history = useHistory();
+  
 
   const fetchMovie = (id) => {
     axios
@@ -27,13 +28,33 @@ function Movie({ addToSavedList }) {
     return <div>Loading movie information...</div>;
   }
 
+  const deleteMovie = e => {
+    e.preventDefault();
+        
+    axios.delete(`http://localhost:5000/api/movies/${params.id}`)
+    .then((res) => {
+        console.log(res.data)
+        setMovieList(res.data);
+        history.push("/")
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+  }
+
   return (
     <div className="save-wrapper">
       <MovieCard movie={movie} />
+      <div>
       <button
-        className="md-button"
-        onClick={() => push(`/update-movie/${movie.id}`)}
+        onClick={() => history.push(`/update-movie/${params.id}`)}
       >Update Movie</button>
+      </div>
+      <br />
+
+      <div>
+        <button onClick={deleteMovie}>Delete Movie</button>
+      </div>
 
       <div className="save-button" onClick={saveMovie}>
         Save
